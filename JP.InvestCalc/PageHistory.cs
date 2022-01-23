afd8ba42ed804536ea931af7e010b1a44288cf8b
XavierAP <2506829+XavierAP@users.noreply.github.com>
2022-01-23 17:35:53 +0100

    Refactor PageHistory to use JP.Utils.Xamarin

--- JP.InvestCalc/PageHistory.cs
+++ JP.InvestCalc/PageHistory.cs
@@ -1,61 +1,42 @@
 ﻿using JP.Utils;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace JP.InvestCalc
 {
-	class PageHistory : ContentPage
+	using Data = List<(long DatabaseId, DateTime Date, string StockName, double Shares, double Flow, double PriceAvg, string Comment)>;
+
+	class PageHistory : TablePage
 	{
 		const ushort
 			nColumns = 6,
-			iColShares = 2,
 			iColPrice = 4;
 
-		readonly FlowEditor data;
+		readonly FlowEditor dataBase;
 		CsvProcessor csv = new CsvProcessor(GetCsvSeparator());
 		readonly string tempCsvFile = Path.Combine(Path.GetTempPath(), "InvestCalc-history.csv");
 
-		readonly List<long> databaseIds;
+		readonly Data dataCache;
 		
-		readonly StackLayout flipLayout = new StackLayout();
-		readonly Grid table = new Grid();
-		readonly Grid buttonsLayout;
-
-		readonly RowDefinition rowLayout = new RowDefinition { Height = GridLength.Auto };
-
-		ushort nButtons = 0;
-
 		public bool HasChanged { get; private set; } = false;
-		private bool IsEmpty => databaseIds.Count < 1;
+
+		readonly Button buttonDelete, buttonCsv;
 
 		public PageHistory(FlowEditor data, params string[] stockNames)
 		{
-			this.data = data;
+			this.dataBase = data;
 
-			this.Content = flipLayout;
-			flipLayout.Children.Add(new ScrollView
-			{
-				Content = table,
-				Orientation = ScrollOrientation.Both,
-				BackgroundColor = Format.BackgroundColor,
-			});
-			for(int i = 0; i < nColumns; i++)
-				table.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Auto });
-			
-			AddHeaders();
-			var rows = data.GetFlowDetailsOrdered(stockNames, DateTime.MinValue, DateTime.Now);
-			databaseIds = new List<long>(rows.Count);
-			foreach(var row in rows)
-				AddRow(row);
+			SetHeaders("Date", "Stock", "Shares", "Flow", "Price", "Comment");
+			AddData(dataCache = data.GetFlowDetailsOrdered(stockNames, DateTime.MinValue, DateTime.Now));
 
-			buttonsLayout = CreateButtonsLayout();
-			AddButton("Delete last", DoDelete).IsEnabled = !IsEmpty;
-			AddButton("CSV", PromptCsvOptions);
+			buttonDelete = AddButtonAtBottom("Delete last", DoDelete);
+			buttonDelete.IsEnabled = !IsEmpty;
+			buttonCsv = AddButtonAtBottom("CSV", PromptCsvOptions);
 
 			new OrientationFlipBehavior(this).SetOrChanged += OnOrientationSetOrChanged;
 
@@ -66,76 +47,30 @@ namespace JP.InvestCalc
 
 		private void OnOrientationSetOrChanged(Orientation orientation)
 		{
-			if(Orientation.Portrait == orientation)
-				flipLayout.Children.Add(buttonsLayout);
-			else
-				flipLayout.Children.Remove(buttonsLayout);
+			buttonDelete.IsVisible = buttonCsv.IsVisible = orientation == Orientation.Portrait;
+		}
+
+		private void AddData(Data data)
+		{
+			for(int i = 0; i < data.Count; i++)
+			{
+				AddRow();
+				foreach(var cell in FormatRow(data[i]))
+					AddCellToCurrentRow(new Label { Text = cell });
+			}
+		}
+
+		private static IEnumerable<string>
+		FormatRow((long DatabaseId, DateTime Date, string StockName, double Shares, double Flow, double PriceAvg, string Comment) row)
+		{
+			yield return row.Date.ToShortDateString();
+			yield return row.StockName;
+			yield return row.Shares.FormatShares();
+			yield return row.Flow.FormatMoneyPlusMinus();
+			yield return row.PriceAvg.FormatMoneyPositive();
+			yield return row.Comment;
 		}
 		
-		private static Grid CreateButtonsLayout()
-		{
-			var layout = new Grid();
-			layout.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
-			var layoutCol = new ColumnDefinition { Width = GridLength.Star };
-			for(int c = 0; c < 2; c++)
-				layout.ColumnDefinitions.Add(layoutCol);
-
-			return layout;
-		}
-
-		private Button AddButton(string text, EventHandler clicked)
-		{
-			var btn = new Button { Text = text };
-			buttonsLayout.Children.Add(btn, nButtons++, 0);
-			btn.Clicked += clicked;
-			return btn;
-		}
-
-		private void AddHeaders()
-		{
-			AddRow("Date", "Stock", "Shares", "Flow", "Price", "Comment");
-			foreach(Label header in table.Children)
-				header.FontAttributes = FontAttributes.Bold;
-		}
-
-		private void AddRow(in (long DatabaseId, DateTime Date, string StockName,
-			double Shares, double Flow, double PriceAvg, string Comment) row )
-		{
-			AddRow(
-				row.Date.ToShortDateString(),
-				row.StockName,
-				row.Shares.FormatShares(),
-				row.Flow.FormatMoneyPlusMinus(),
-				row.PriceAvg.FormatMoneyPositive(),
-				row.Comment);
-
-			databaseIds.Add(row.DatabaseId);
-		}
-
-		private void AddRow(string date, string stockName, string shares, string flow, string price, string comment)
-		{
-			int irow = table.RowDefinitions.Count;
-			int icol = 0;
-			table.RowDefinitions.Add(rowLayout);
-
-			table.Children.Add(new Label { Text = date },
-				icol++, irow);
-			table.Children.Add(new Label { Text = stockName },
-				icol++, irow);
-			Debug.Assert(icol == iColShares);
-			table.Children.Add(new Label { Text = shares },
-				icol++, irow);
-			table.Children.Add(new Label { Text = flow },
-				icol++, irow);
-			Debug.Assert(icol == iColPrice);
-			table.Children.Add(new Label { Text = price },
-				icol++, irow);
-			table.Children.Add(new Label { Text = comment },
-				icol++, irow);
-
-			Debug.Assert(icol == nColumns);
-		}
-
 		#region Delete button
 
 		private async void DoDelete(object sender, EventArgs args)
@@ -144,30 +79,19 @@ namespace JP.InvestCalc
 			if(!confirmed) return;
 
 			DeleteLastRecordFromDatabase();
-			DeleteLastRecordFromUI();
+			DeleteLastRow();
 			if(IsEmpty) await Close();
 		}
 
 		private void DeleteLastRecordFromDatabase()
 		{
-			var lastIndex = databaseIds.Count - 1;
-			databaseIdCache[0] = databaseIds[lastIndex];
-			databaseIds.RemoveAt(lastIndex);
-			data.DeleteFlows(databaseIdCache);
+			var lastIndex = dataCache.Count - 1;
+			DeleteLastCache[0] = dataCache[lastIndex].DatabaseId;
+			dataCache.RemoveAt(lastIndex);
+			dataBase.DeleteFlows(DeleteLastCache);
 			HasChanged = true;
 		}
-		readonly long[] databaseIdCache = new long[1];
-
-		private void DeleteLastRecordFromUI()
-		{
-			var lastIndex = table.Children.Count - 1;
-			var lastIndexToRemain = lastIndex - nColumns;
-			for(var i = lastIndex; i > lastIndexToRemain; --i)
-				table.Children.RemoveAt(i);
-
-			var rows = table.RowDefinitions;
-			rows.RemoveAt(rows.Count - 1);
-		}
+		readonly long[] DeleteLastCache = new long[1];
 
 		#endregion
 
@@ -205,24 +129,18 @@ namespace JP.InvestCalc
 				File = new ShareFile(tempCsvFile, "text/csv"),
 			});
 		}
-
 		private IEnumerable<List<string>> GetCsvData()
 		{
-			const int nValues = nColumns - 1;
-			var row = new List<string>(nValues);
-			for(int i = nColumns; // Don't start at 0: skip headers
-				i < table.Children.Count;
-				i += nColumns, row.Clear() )
+			var rowCache = new List<string>(nColumns - 1);
+			for(int r = 0; r < dataCache.Count; r++, rowCache.Clear())
 			{
-				for(int c = 0; c < nColumns; c++)
-				{
-					if(iColPrice == c) continue; // price = flow/shares, displayed for info
-					row.Add((table.Children[i + c] as Label).Text);
-				}
-				Debug.Assert(nValues == row.Count);
-				yield return row;
+				rowCache.AddRange(FormatRow(dataCache[r]).Where(
+					(cell, index) => iColPrice != index )); // skip dependent variable
+				
+				yield return rowCache;
 			}
 		}
+
 		private async Task ImportCsvFile()
 		{
 			var file = await FilePicker.PickAsync();
@@ -230,7 +148,7 @@ namespace JP.InvestCalc
 
 			int n;
 			try {
-				n = data.ImportFlows(File.ReadAllText(file.FullPath), csv);
+				n = dataBase.ImportFlows(File.ReadAllText(file.FullPath), csv);
 			}
 			catch(DataException err)
 			{
