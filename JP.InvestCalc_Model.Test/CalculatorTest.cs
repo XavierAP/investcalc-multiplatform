f51334ef3f2f709d9915221c8d86ec5da57e6cca
XavierAP <2506829+XavierAP@users.noreply.github.com>
2023-08-06 06:04:30 +0200

    CalculatorTest literal style

--- JP.InvestCalc_Model.Test/CalculatorTest.cs
+++ JP.InvestCalc_Model.Test/CalculatorTest.cs
@@ -5,9 +5,9 @@
 	{
 		const double tolerance = 1e-9;
 
-		[TestCase(1_001.00, new double[] { -1_000 }, 1.00, 0.001)]
-		[TestCase(   50.00, new double[] { -100, 25 }, -25.00, -0.25)]
-		[TestCase(   15.00, new double[] { -10, 1, -10 }, -4.00, -0.20)]
+		[TestCase(1_001.00, new double[] { -1_000 }, 1.00, 1.00/1_000)]
+		[TestCase(   50.00, new double[] { -100, 25 }, -25.00, -25.00/100)]
+		[TestCase(   15.00, new double[] { -10, 1, -10 }, -4.00, -4.00/20)]
 		[TestCase(   14.00, new double[] { -8, 1, -4, 2 }, 5.00, 5.00/12)]
 
 		public void CalculateGain(double presentValue, IEnumerable<double> cashFlows,
